6339d703f008c6f856450d14491fed818f64704b
fracs31 <pist.fracs95@gmail.com>
2023-04-27 14:21:05 +0200

    ADD: Technology's seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology; //model
+use Illuminate\Support\Str; //str
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ["HTML", "CSS", "JavaScript", "Bootstrap", "Vue", "PHP", "MySQL", "Laravel"]; //tecnologie del progetto
+        //Ciclo
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology(); //nuova tecnologia
+            $newTechnology->name = $technology; //nome
+            $newTechnology->slug = Str::slug($technology); //slug
+            $newTechnology->save(); //invio i dati al database
+        }
     }
 }
