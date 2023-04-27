6fb2f809cba6c2aec0b473e8c1c191b6bfc5919a
fracs31 <pist.fracs95@gmail.com>
2023-04-27 14:22:08 +0200

    ADD: TechnologySeeder to the DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TypeSeeder::class,
             ProjectSeeder::class,
+            TechnologySeeder::class
         ]);
     }
 }
